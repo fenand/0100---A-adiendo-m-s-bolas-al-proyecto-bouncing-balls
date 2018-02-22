b07712dc0b37e37a31f766766e0ff34b6da9f599
Fenand <garciaf4c@gmail.com>
2018-02-22 16:00:51 +0100

    Se modifica ballDemo para crear tantas bolas como queramos por parametro y se mueven correctamente

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -10,13 +11,16 @@ import java.awt.Color;
 
 public class BallDemo   
 {
-    private Canvas myCanvas;
+    private Canvas myCanvas;   
+    // creamos un arraylist de bolas creadas 
+    private ArrayList<BouncingBall> misBolas;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        misBolas = new ArrayList<>();
     }
 
     /**
@@ -28,32 +32,30 @@ public class BallDemo
         myCanvas.setVisible(true);
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
-
         //creamos tantas bolas como el parametro numeroDeBolasPorPantalla
-
         for(int i = 0 ; i<numeroDeBolasPorPantalla ; i++){
             //para crear bolas en posiciones distintas modificamos el primer parametro al crear
             // la bola y que se modifique segun la i que es el contador
-            BouncingBall ball = new BouncingBall(50+(5*i), 50, 16, Color.BLUE, ground, myCanvas);
+            BouncingBall ball = new BouncingBall(50+(50*i), 50, 16, Color.BLUE, ground, myCanvas);
             ball.draw();
-
+            misBolas.add(ball);
         }
-
         // crate and show the balls
         // BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         // ball.draw();
         // BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
         // ball2.draw();
         // make them bounce
-        // boolean finished =  false;
-        // while(!finished) {
-            // myCanvas.wait(50);           // small delay
-            // ball.move();
-            // //ball2.move();
-            // // stop once ball has travelled a certain distance on x axis
+        boolean finished =  false;
+        while(!finished) {
+            myCanvas.wait(50);           // small delay
+            for(int i = 0 ; i<numeroDeBolasPorPantalla; i++){
+             misBolas.get(i).move();              
+            }
+            // stop once ball has travelled a certain distance on x axis
             // if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
                 // finished = true;
             // }
-        // }
+        }
     }
 }
